a1a1e4611929cc6d1bee2616bbd8005bc9a01f96
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2015-11-30 19:46:02 -0500

    attempt to upgrade versions

--- tdd.fsx
+++ tdd.fsx
@@ -17,12 +17,16 @@ open MemoryWayback.Persistence
 open MemoryWayback.MediaLibraryUpdate
 open MemoryWayback.Tests.MemoryPersistence
 
+module MyNewStuff =
+  let add x y =
+    x + y
+
 //[<TestFixture>]
 type ``tdd tests``() =
 
   [<Fact>]
   member x.``next module``() =
-    2 + 2 |> should equal 5
+    MyNewStuff.add 2 2 |> should equal 4
 
 
 Tdd_support.runTests (``tdd tests``())
