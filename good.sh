51fa7cabfcdaf9d8d32bcb8197a8310d939453fd
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2016-06-08 22:36:42 -0400

    use debug for good.sh

--- good.sh
+++ good.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/Library/Frameworks/Mono.framework/Commands/mono --debug "/Users/will/home/prog/memory_wayback/packages/xunit.runner.console/tools/xunit.console.exe" "/Users/will/home/prog/TimestampedJsonLogFilter/tests/TimestampedJsonLogFilter.Tests/bin/Release/TimestampedJsonLogFilter.Tests.dll" -noappdomain -parallel none
+/Library/Frameworks/Mono.framework/Commands/mono --debug "/Users/will/home/prog/memory_wayback/packages/xunit.runner.console/tools/xunit.console.exe" "/Users/will/home/prog/TimestampedJsonLogFilter/tests/TimestampedJsonLogFilter.Tests/bin/Debug/TimestampedJsonLogFilter.Tests.dll" -noappdomain -parallel none
